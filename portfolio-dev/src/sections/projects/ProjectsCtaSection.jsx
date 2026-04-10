12d60e6234450a6c446136f0f27fe42b28d6dafe
Lucas Corrêa <lucas.engsoftwareee@gmail.com>
2026-04-10 14:47:10 -0300

    strucuture of CtaSection - Project

--- portfolio-dev/src/sections/projects/ProjectsCtaSection.jsx
+++ portfolio-dev/src/sections/projects/ProjectsCtaSection.jsx
@@ -1,5 +1,18 @@
 function ProjectsCtaSection() {
-  return <section></section>;
+  return (
+    <>
+      <div className="font-inter  text-sm px-6 mt-12">
+        <div className="border border-gray-500 rounded-md p-4">
+          <p className="text-gray-500">
+            quer ver mais? tudo está no <a className="text-black">GitHub</a>
+          </p>
+          <button className="rounded-xl py-3 border border-gray-400 w-full mt-3 ">
+            ⌥ ver perfil no github
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ProjectsCtaSection;
