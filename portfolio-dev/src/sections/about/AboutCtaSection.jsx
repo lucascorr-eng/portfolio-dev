ece186b0ecd30f7e3334513bc4511564217d683d
Lucas Corrêa <lucas.engsoftwareee@gmail.com>
2026-04-10 17:01:36 -0300

    structure ctaSection - About

--- portfolio-dev/src/sections/about/AboutCtaSection.jsx
+++ portfolio-dev/src/sections/about/AboutCtaSection.jsx
@@ -1,5 +1,24 @@
 function AboutCtaSection() {
-  return <section></section>;
+  return (
+    <>
+      <div className="font-inter  text-sm px-6 mt-12">
+        <div className="bg-mist-100 px-4 py-6 rounded-xl">
+          <div className="grid mb-4">
+            <h2 className="font-bold text-xl">bora trocar uma ideia?</h2>
+            <p className="text-gray-500">
+              aberto para projetos interessantes, freelas ou um papo sobre
+              tecnologia
+            </p>
+          </div>
+          <div className="font-firaCode">
+            <button className="mt-3 rounded-xl py-3 bg-black text-white w-full">
+              entrar em contato
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AboutCtaSection;
