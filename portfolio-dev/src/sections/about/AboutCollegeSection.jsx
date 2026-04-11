5c3f49d255110cc99f81aef63bd399756b3893fb
Lucas Corrêa <lucas.engsoftwareee@gmail.com>
2026-04-11 12:11:57 -0300

    structure of collegeSection - About

--- portfolio-dev/src/sections/about/AboutCollegeSection.jsx
+++ portfolio-dev/src/sections/about/AboutCollegeSection.jsx
@@ -1,5 +1,100 @@
 function AboutCollegeSection() {
-  return <section></section>;
+  return (
+    <>
+      <div className="font-inter text-sm px-6 my-10">
+        <h3 className="font-firaCode text-base text-gray-500 mb-6">
+          FORMAÇÃO ACADÊMICA
+        </h3>
+
+        <div className="grid border border-gray-500 rounded-lg p-4">
+          <div className="flex justify-between">
+            <h2 className="text-lg">Engenharia de Software</h2>
+            <div className="flex items-center gap-3 bg-green-100 rounded-xl px-4">
+              <div className="h-3 w-3 bg-green-300 rounded-full "></div>
+              <p>cursando</p>
+            </div>
+          </div>
+          <p className="text-gray-500">Universidade de Franca — Hibrido</p>
+          <div className="flex flex-wrap font-firaCode text-green-900 gap-2 gap-x-3 mt-4">
+            <div className="bg-green-100 rounded-lg px-2">
+              <p>algoritmos</p>
+            </div>
+            <div className="bg-green-100 rounded-lg px-2">
+              <p>arquitetura</p>
+            </div>
+            <div className="bg-green-100 rounded-lg px-2">
+              <p>banco de dados</p>
+            </div>
+            <div className="bg-green-100 rounded-lg px-2">
+              <p>requisitos</p>
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <div className="flex justify-between">
+              <p className="text-gray-500">progresso do curso </p>
+              <p className="text-green-900">60% concluído</p>
+            </div>
+            <div className="bg-gray-300 h-1 w-full rounded-2xl">
+              <div
+                className="bg-green-500 h-full rounded-2xl mt-3"
+                style={{ width: "60%" }}
+              ></div>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap gap-2 mt-4">
+            <div className="text-green-900 bg-green-100 px-2 py-1 rounded-md">
+              <p>1º</p>
+            </div>
+            <div className="text-green-900 bg-green-100 px-2 py-1 rounded-md">
+              <p>2º</p>
+            </div>
+            <div className="text-green-900 bg-green-100 px-2 py-1 rounded-md">
+              <p>3º</p>
+            </div>
+            <div className="text-green-900 bg-green-100 px-2 py-1 rounded-md">
+              <p>4º</p>
+            </div>
+            <div className="text-green-900 bg-green-100 px-2 py-1 rounded-md">
+              <p>5º</p>
+            </div>
+            <div className="text-green-900 bg-green-100 px-2 py-1 rounded-md">
+              <p>6º</p>
+            </div>
+            <div className="text-green-900 bg-green-100 px-2 py-1 rounded-md">
+              <p>7º</p>
+            </div>
+            <div className="text-green-900 bg-green-100 px-2 py-1 rounded-md">
+              <p>8º</p>
+            </div>
+          </div>
+
+          <div className="flex gap-6 mt-2">
+            <div className="flex items-center gap-2">
+              <div className="w-3 h-3 bg-green-700 rounded-sm"></div>
+              <p>cursando</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="w-3 h-3 bg-green-200 border border-green-400 rounded-sm"></div>
+              <p>concluído</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="w-3 h-3 bg-mist-200  rounded-sm"></div>
+              <p>próximo</p>
+            </div>
+          </div>
+
+          <div className="text-gray-500 flex justify-between mt-4">
+            <p>ingresso: 2022 </p>
+            <p>
+              conclusão: <span className="text-black">2025/2</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AboutCollegeSection;
--- portfolio-dev/src/sections/about/AboutCuriositySection.jsx
+++ portfolio-dev/src/sections/about/AboutCuriositySection.jsx
@@ -6,23 +6,23 @@ function AboutCuriositySection() {
       <DividerLine />
       <div className="font-inter text-sm px-6 text-gray-500 my-6">
         <h3 className="font-firaCode text-base">INTERESSES & CURIOSIDADES</h3>
-        <div className="mt-4 grid grid-cols-[auto_auto_auto] gap-2 gap-x-6">
-          <div className="flex justify-center py-1 rounded-md border border-gray-500">
+        <div className="mt-4 flex flex-wrap gap-2 gap-x-4">
+          <div className="flex justify-center px-1 py-1 rounded-md border border-gray-500">
             <p>⚙ arquitetura</p>
           </div>
-          <div className="flex justify-center py-1 rounded-md border border-gray-500">
+          <div className="flex justify-center px-1 py-1 rounded-md border border-gray-500">
             <p>◎ open source</p>
           </div>
-          <div className="flex justify-center py-1 rounded-md border border-gray-500">
+          <div className="flex justify-center px-1 py-1 rounded-md border border-gray-500">
             <p>△ cloud & devops</p>
           </div>
-          <div className="flex justify-center py-1 rounded-md border border-gray-500">
+          <div className="flex justify-center px-1 py-1 rounded-md border border-gray-500">
             <p>◈ ui / ux</p>
           </div>
-          <div className="flex justify-center py-1 rounded-md border border-gray-500">
+          <div className="flex justify-center px-1 py-1 rounded-md border border-gray-500">
             <p>◻ inteligência artificial</p>
           </div>
-          <div className="flex justify-center py-1 rounded-md border border-gray-500">
+          <div className="flex justify-center px-1 py-1 rounded-md border border-gray-500">
             <p>● xadrez</p>
           </div>
         </div>
